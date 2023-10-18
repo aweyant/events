2777194e9a4927b9a7cfa9f981aa505dddc9e30e
aweyant <aweyant@ucsd.edu>
2023-10-17 17:02:17 -0700

    first minimal code for create_events_from_vector(); setting up documentation and formal testing remains

--- R/create_events_from_vector.R
+++ R/create_events_from_vector.R
@@ -14,6 +14,28 @@
 #' create_events_from_vector(v, 100)
 #' # ... some output ...
 create_events_from_vector <- function(v, threshold) {
-  # . . .
-  return(v)
+  temp_rle <- rle(ifelse(v > threshold,1,0))
+  end_indices <- cumsum(temp_rle$lengths)[which(temp_rle$values != 0)] # end
+  #' indices of runs
+  lengths <- temp_rle$lengths[which(temp_rle$values == 1)] # durations
+  start_indices <- (end_indices - lengths) + 1 # start indices of runs
+
+  return(cbind(data.frame(start_index = start_indices,
+                          length = lengths),
+               do.call(rbind,
+                       lapply(X = seq_along(start_indices),
+                              FUN = function(i){
+                                vals = (v-threshold)[start_indices[i]:end_indices[i]]
+                                data.frame(magnitude = sum(vals),
+                                           maximum = max(vals))
+                              }))
+  ))
 }
+
+# scratch
+v <- c(333,587,0,201,82,225,70,62,47,101,187)
+threshold <- 100
+create_events_from_vector(v, threshold)
+
+
+
