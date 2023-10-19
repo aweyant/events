fb618c97d5004e1926695b95c9f529435de30aa7
aweyant <aweyant@ucsd.edu>
2023-10-19 14:29:59 -0700

    polishing hurdle geometric utility functions

--- R/hgeom.R
+++ R/hgeom.R
@@ -3,16 +3,44 @@
 #' Density, distribution function, quantile function and random generation for
 #' the 1-inflated geometric distribution with parameters prob_p and prob_q.
 #'
-#' @param x
-#' @param prob_p
-#' @param prob_q
-#' @param log
+#' @param x,q vector of quantiles representing the number of failures in a
+#' sequence of Bernoulli trials before success occurs.
+#' @param p vector of probabilities.
+#' @param n number of observations. If length(n) > 1, the length is taken to be
+#' the number required.
+#' @param prob_p probability of success in each trial after the first trial, if
+#' the first trial was not a success. \eqn{0 <} prob_p \eqn{\le 1}.
+#' @param prob_q probability of success in the first trial. \eqn{0 \le} prob_q \eqn{\le 1}
+#' @param log,log.p logical; if TRUE, probabilities p are given as log(p).
+#' @param lower.tail logical; if TRUE (default), probabilities are
+#' \eqn{P[X \le x]}, otherwise, \eqn{P[X > x]}.
 #'
 #' @return
-#' @export
+#'  hgeom is a dummy function to keep the hurdle geometric documentation
+#'  together.
 #'
+#'  dhgeom gives the density, phgeom gives the distribution function, qhgeom
+#'  gives the quantile function, and rhgeom generates random deviates.
+#'
+#'  Invalid prob_p or prob_q will result in return value NaN, with a warning.
+#'
+#'  rhgeom returns a vector of type integer unless generated values exceed the
+#'  maximum representable integer when double values are returned since R
+#'  version 4.0.0.
+#'
+#' @rdname hgeom
+#' @export
+#' @usage hgeom(x, q, p, n, prob_p, prob_q, log, log.p, lower.tail)
+hgeom <- function(x, q, p, n, prob_p, prob_q, log, log.p, lower.tail) {
+  list(x, q, p, n, prob_p, prob_q, log, log.p, lower.tail)
+  }
+
+#' @rdname hgeom
 #' @examples
+#' plot(dhgeom(x = 1:25, prob_p = 0.3, prob_q = 0.6))
+#' @export
 dhgeom <- function(x, prob_p, prob_q, log = FALSE){
+  hgeom_check_args(prob_p, prob_q)
   prob_out <- double(length(x))
   prob_out[which(x < 1)] <- 0
   prob_out[which(x == 1)] <- prob_q
@@ -20,7 +48,10 @@ dhgeom <- function(x, prob_p, prob_q, log = FALSE){
   if(log) {return(log(prob_out))}
   prob_out
 }
+#' @rdname hgeom
+#' @export
 phgeom <- function(q, prob_p, prob_q, lower.tail = TRUE, log.p = FALSE){
+  hgeom_check_args(prob_p, prob_q)
   nep <- double(length(q))
   nep[which(q < 1)] <- 0
   nep[which(q == 1)] <- prob_q
@@ -31,9 +62,39 @@ phgeom <- function(q, prob_p, prob_q, lower.tail = TRUE, log.p = FALSE){
   if(log.p) {return(log(prob_out))}
   prob_out
 }
+#' @rdname hgeom
+#' @export
 qhgeom <- function(p, prob_p, prob_q, lower.tail = TRUE, log.p = FALSE){
+  hgeom_check_args(prob_p, prob_q)
+  x <- integer(length(p))
+  if(log.p) {p <- exp(p)}
+  if(!lower.tail) {p <- 1-p}
 
+  x[which(p < prob_q)] <- 1
+  x[which(p > prob_q)] <- ceiling(1 + log(1 - (p[which(p > prob_q)]-prob_q)/(1-prob_q))/log(1 - prob_p))
+  x
 }
+#' @rdname hgeom
+#' @export
 rhgeom <- function(n, prob_p, prob_q){
+  hgeom_check_args(prob_p, prob_q)
+  1 + (stats::rbinom(n = n, size = 1, prob = 1 - prob_q)) * (1 + stats::rgeom(n = n, prob = prob_p))
+}
 
+hgeom_check_args <- function(prob_p, prob_q) {
+  prob_p_invalid <- FALSE
+  if(prob_p <= 0 | prob_p > 1) {
+    warning("Prob_p must be greater than 0 and less than or equal to 1. Returning NaN.")
+    prob_p_invalid <- TRUE
+  }
+  prob_q_invalid <- FALSE
+  if(prob_q < 0 | prob_q > 1) {
+    warning("Prob_q must be greater than or equal to 0 and less than or equal to 1. Returning NaN.")
+    prob_q_invalid <- TRUE
+  }
+  if(prob_p_invalid | prob_q_invalid) {
+    call <- rlang::expr(return(NaN))
+    rlang::eval_bare(call, env = parent.frame())
+  }
+  NULL
 }
--- tests/testthat/test-hgeom.R
+++ tests/testthat/test-hgeom.R
@@ -5,3 +5,17 @@ test_that("pmf and cdf conform", {
   expect_equal(sum(dhgeom(x = 0:n, prob_p = p, prob_q = q)),
                phgeom(q = n, prob_p = p, prob_q = q))
 })
+
+test_that("quantile is truly inverse of cdf", {
+  p = c(0.6, 0.75, 0.99)
+  prob_p <- 0.42
+  prob_q <- 0.7
+  # The quantile x is defined to be minimal x s.t. P(X<=x) >= p
+  purported_quantiles <- qhgeom(p = p, prob_p = prob_p, prob_q = prob_q)
+  inequality_holds <- as.logical(prod(phgeom(q = purported_quantiles, prob_p = prob_p, prob_q = prob_q) >= p))
+
+  minimality_holds <- length(p) == sum(phgeom(q = purported_quantiles - 1, prob_p = prob_p, prob_q = prob_q) < p)
+
+  expect_identical(inequality_holds & minimality_holds,
+                   TRUE)
+})
