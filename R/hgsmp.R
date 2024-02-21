d278c1f5a03a177e41e2344bd3a01846da06f0d9
aweyant <aweyant@ucsd.edu>
2024-02-21 13:13:26 -0800

    skeleton of hgsmp file

---
+++ R/hgsmp.R
@@ -0,0 +1,29 @@
+rhgsmp <- function(n, alpha, beta, prob_p, prob_q, rounding = FALSE) {
+
+}
+
+phgsmp <- function(
+    x_lower = (1e-10)/beta,
+    x_upper = Inf,
+    y_lower = (1e-10)/beta,
+    y_upper = Inf,
+    n_lower = 1,
+    n_upper = Inf,
+    alpha,
+    beta,
+    prob_p,
+    prob_q,
+    x_lims = NULL,
+    y_lims = NULL,
+    n_lims = NULL) {
+}
+
+phgsmpdt <- function(q, alpha, beta, prob_p, prob_q, lower.tail = TRUE) {
+
+}
+
+cdf_hgsmp <- function(x, y, n,
+                      alpha, beta, prob_p, prob_q,
+                      tol = 10^(-6),
+                      max_N = 100) {
+}
